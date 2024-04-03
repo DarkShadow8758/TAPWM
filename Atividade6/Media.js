2ed1c26ae03f032750e02736c9833d70d67c82fb
Gabriel Sales Dorea <133690325+DarkShadow8758@users.noreply.github.com>
2024-04-03 01:59:45 -0300

    Add files via upload

---
+++ Atividade6/Media.js
@@ -0,0 +1,9 @@
+var nome = prompt("Digite seu nome");
+var nota1 = parseFloat(prompt("Digite sua primeira nota"));
+var nota2 = parseFloat(prompt("Digite sua segunda nota"));
+var nota3 = parseFloat(prompt("Digite sua terceira nota"));
+
+
+var notaTotal = nota1 + nota2 + nota3;
+alert("Olá " + nome + ", você ficou com nota: " + notaTotal)
+
---
+++ Atividade6/Operacoes.js
@@ -0,0 +1,11 @@
+var n1 = parseFloat(prompt("Digite o primeiro numero:"));
+var n2 = parseFloat(prompt("Digite o segundo numero:"));
+
+var soma = n1 + n2;
+var subtracao = n1 - n2;
+var multiplicacao = n1 * n2;
+var divisao = n1 / n2;
+var resto = n1 % n2;
+
+alert("Soma = " + soma + "\nSubtração = " + subtracao + "\nMultiplicação = " + multiplicacao + "\nDivisão = " + divisao + "\nResto = " + resto)
+
