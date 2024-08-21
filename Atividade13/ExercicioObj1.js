7d327777588b21516fba0af8b396f1abf1d4825c
Gabriel Sales Dorea <133690325+DarkShadow8758@users.noreply.github.com>
2024-08-21 15:42:48 -0300

    Add files via upload

---
+++ Atividade13/ExercicioObj1.js
@@ -0,0 +1,18 @@
+var Aluno1 = new Object();
+Aluno1.ra = "00340987";
+Aluno1.nome = "Clorivaldo";
+
+alert("RA: "+ Aluno1.ra +"\nNome: " + Aluno1.nome);
+
+var Aluno2 = {};
+Aluno2.ra = "00547487";
+Aluno2.nome = "Antolino";
+
+alert("RA: "+ Aluno2.ra +"\nNome: " + Aluno2.nome);
+
+var Aluno3 ={
+    ra: "09876543",
+    nome: "Ludovico"
+}
+
+alert("RA: "+ Aluno3.ra +"\nNome: " + Aluno3.nome)
